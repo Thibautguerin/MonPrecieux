2a7b3288fb3fa08f3dd6832683e10a1ba493b753
Thibautguerin <thibaut.guerin@edu.devinci.fr>
2021-09-08 19:24:30 +0200

    Add Player animations

--- MonPrecieux/Assets/Scripts/PlayerMovement.cs
+++ MonPrecieux/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,120 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+
+    [Header("Animations Speed")]
+    public float animationSpeed = 3;
+    public float animationToIdleSpeed = 3;
+    public float idleAnimationSpeed = 0.2f;
+
+    [Header("Animations Parameters")]
+    public float maxRotation = 10;
+    public float minIdleScale = 0.95f;
+    public float maxIdleScale = 1.05f;
+
+    [Space]
+    public Transform rotator;
+
     private float h;
     private float v;
     private Rigidbody2D playerbody;
-    public Transform rotator;
+
+    private float currentAnimRotation;
+    private bool toggleAnimation;
+    private float currentAnimScale;
+
+    private Vector3 scaleSave;
+
     void Start()
     {
         playerbody = GetComponent<Rigidbody2D>();
+        scaleSave = transform.localScale;
     }
+
     void Update()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
         if (!UI.instance.isPaused) orientation();
+
+        if (playerbody.velocity.magnitude > 0)
+        {
+            if (toggleAnimation)
+            {
+                if (currentAnimRotation == 1)
+                {
+                    toggleAnimation = false;
+                    currentAnimRotation = Mathf.Max(currentAnimRotation - Time.deltaTime * animationSpeed * playerbody.velocity.magnitude, -1);
+                }
+                else
+                {
+                    currentAnimRotation = Mathf.Min(currentAnimRotation + Time.deltaTime * animationSpeed * playerbody.velocity.magnitude, 1);
+                }
+            }
+            else
+            {
+                if (currentAnimRotation == -1)
+                {
+                    toggleAnimation = true;
+                    currentAnimRotation = Mathf.Min(currentAnimRotation + Time.deltaTime * animationSpeed * playerbody.velocity.magnitude, 1);
+                }
+                else
+                {
+                    currentAnimRotation = Mathf.Max(currentAnimRotation - Time.deltaTime * animationSpeed * playerbody.velocity.magnitude, -1);
+                }
+            }
+
+            if (currentAnimScale > 1)
+            {
+                currentAnimScale = Mathf.Max(currentAnimScale - Time.deltaTime * idleAnimationSpeed, 1);
+            }
+            else if (currentAnimScale < 1)
+            {
+                currentAnimScale = Mathf.Min(currentAnimScale + Time.deltaTime * idleAnimationSpeed, 1);
+            }
+        }
+        else
+        {
+            if (toggleAnimation)
+            {
+                if (currentAnimScale == maxIdleScale)
+                {
+                    toggleAnimation = false;
+                    currentAnimScale = Mathf.Max(currentAnimScale - Time.deltaTime * idleAnimationSpeed, minIdleScale);
+                }
+                else
+                {
+                    currentAnimScale = Mathf.Min(currentAnimScale + Time.deltaTime * idleAnimationSpeed, maxIdleScale);
+                }
+            }
+            else
+            {
+                if (currentAnimScale == minIdleScale)
+                {
+                    toggleAnimation = true;
+                    currentAnimScale = Mathf.Min(currentAnimScale + Time.deltaTime * idleAnimationSpeed, maxIdleScale);
+                }
+                else
+                {
+                    currentAnimScale = Mathf.Max(currentAnimScale - Time.deltaTime * idleAnimationSpeed, minIdleScale);
+                }
+            }
+
+            if (currentAnimRotation > 0)
+            {
+                currentAnimRotation = Mathf.Max(currentAnimRotation - Time.deltaTime * animationToIdleSpeed, 0);
+            }
+            else if (currentAnimRotation < 0)
+            {
+                currentAnimRotation = Mathf.Min(currentAnimRotation + Time.deltaTime * animationToIdleSpeed, 0);
+            }
+        }
+
+        Quaternion newRotation = new Quaternion();
+        newRotation.eulerAngles = new Vector3(transform.rotation.x, 0, currentAnimRotation * maxRotation);
+        transform.rotation = newRotation;
+
+        transform.localScale = scaleSave * currentAnimScale;
     }
     private void FixedUpdate()
     {
@@ -31,6 +132,9 @@ public class PlayerMovement : MonoBehaviour
             mousePosition.y - rotator.position.y
         );
         rotator.up = direction;
-        transform.localScale = Input.mousePosition.x < Screen.width / 2 ? new Vector3(-1, 1, 1) : Vector3.one;
+
+        //Vector3 newScale = Input.mousePosition.x < Screen.width / 2 ? new Vector3(-1, 1, 1) : Vector3.one;
+        scaleSave = Input.mousePosition.x < Screen.width / 2 ? new Vector3(-1, 1, 1) : Vector3.one;
+        //transform.localScale = Input.mousePosition.x < Screen.width / 2 ? new Vector3(-1, 1, 1) : Vector3.one;
     }
 }
