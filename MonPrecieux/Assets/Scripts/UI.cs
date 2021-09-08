b9e1a630fc998670ae4cc005ef141b6aff711da6
jqntn <jqntn88@gmail.com>
2021-09-08 11:08:11 +0200

    feat(ui): add how to play panel

--- MonPrecieux/Assets/Scripts/UI.cs
+++ MonPrecieux/Assets/Scripts/UI.cs
@@ -8,37 +8,47 @@ class UI : MonoBehaviour
     public bool isMenued;
     public bool isSettinged;
     public bool isCredited;
+    public bool isHTP;
+    public bool showHTP;
     [Header("Layers")]
     public GameObject L_InGame;
     public GameObject L_Pause;
     public GameObject L_Menu;
     public GameObject L_Settings;
     public GameObject L_Credits;
+    public GameObject L_HTP;
     [Header("Toggles")]
     public Toggle Toggle_Global;
     public Toggle Toggle_Music;
     public Toggle Toggle_SFX;
+    public Toggle Toggle_HTP;
     [Header("Sliders")]
     public Slider Slider_Global;
     public Slider Slider_Music;
     public Slider Slider_SFX;
+    void Start() { showHTP = PlayerPrefs.GetInt("showHTP", 1) == 1 ? true : false; }
+    void OnDestroy() { PlayerPrefs.SetInt("showHTP", showHTP ? 1 : 0); }
     void Update()
     {
         Time.timeScale = isPaused ? 0 : 1;
-        L_InGame.SetActive(!isPaused && !isMenued);
+        L_InGame.SetActive(!isPaused && !isMenued && !isHTP);
         L_Pause.SetActive(isPaused && !isSettinged && !isCredited);
-        L_Menu.SetActive(isMenued && !isSettinged && !isCredited);
+        L_Menu.SetActive(isMenued && !isSettinged && !isCredited && !isHTP);
         L_Settings.SetActive(isSettinged);
         L_Credits.SetActive(isCredited);
-        if (!isMenued && !isSettinged && !isCredited && Input.GetKeyDown(KeyCode.Escape)) SwitchIsPaused();
+        L_HTP.SetActive(isHTP);
+        if (!isMenued && !isSettinged && !isCredited && !isHTP && Input.GetKeyDown(KeyCode.Escape)) SwitchIsPaused();
         if (isSettinged && Input.GetKeyDown(KeyCode.Escape)) SwitchIsSettinged();
         if (isCredited && Input.GetKeyDown(KeyCode.Escape)) SwitchIsCredited();
+        if (isHTP && Input.GetKeyDown(KeyCode.Escape)) SwitchIsHTP();
         Toggle_Global.onValueChanged.AddListener(delegate { AudioManager.instance.isGlobalOn = Toggle_Global.isOn; });
         Toggle_Music.onValueChanged.AddListener(delegate { AudioManager.instance.isMusicOn = Toggle_Music.isOn; });
         Toggle_SFX.onValueChanged.AddListener(delegate { AudioManager.instance.isSfxOn = Toggle_SFX.isOn; });
+        Toggle_HTP.onValueChanged.AddListener(delegate { showHTP = Toggle_HTP.isOn; });
         Toggle_Global.isOn = AudioManager.instance.isGlobalOn;
         Toggle_Music.isOn = AudioManager.instance.isMusicOn;
         Toggle_SFX.isOn = AudioManager.instance.isSfxOn;
+        Toggle_HTP.isOn = showHTP;
         Slider_Global.onValueChanged.AddListener(delegate { AudioManager.instance.globalVolume = Slider_Global.value; });
         Slider_Music.onValueChanged.AddListener(delegate { AudioManager.instance.musicVolume = Slider_Music.value; });
         Slider_SFX.onValueChanged.AddListener(delegate { AudioManager.instance.sfxVolume = Slider_SFX.value; });
@@ -52,6 +62,7 @@ class UI : MonoBehaviour
     public void SwitchIsPaused() { isPaused ^= true; }
     public void SwitchIsSettinged() { isSettinged ^= true; }
     public void SwitchIsCredited() { isCredited ^= true; }
+    public void SwitchIsHTP() { isHTP ^= true; }
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -59,8 +70,15 @@ class UI : MonoBehaviour
     }
     public void Play()
     {
-        SceneManager.LoadScene("Game");
+        if (showHTP) isHTP = true;
+        else StartGame();
+    }
+    public void StartGame()
+    {
         isMenued = false;
+        isHTP = false;
+        showHTP = false;
+        SceneManager.LoadScene("Game");
     }
     public void Menu()
     {
