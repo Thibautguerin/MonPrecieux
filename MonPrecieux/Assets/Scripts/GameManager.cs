5c27558dc306881e1b9063cff248c9695a72d027
jqntn <jqntn88@gmail.com>
2021-09-09 16:48:53 +0200

    feat(ui): add percentage

--- MonPrecieux/Assets/Scripts/GameManager.cs
+++ MonPrecieux/Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public Text T_Perc;
-    int hasBurned;
+    public TextMeshProUGUI T_Perc;
+    [Range(1, 2)]
+    public float bumpScaleFactor;
+    [Range(0, 1)]
+    public float bumpSpeed;
+    float hasBurned;
     Flamable[] flammables;
     void Awake() { if (instance == null) { instance = this; DontDestroyOnLoad(gameObject); } else Destroy(gameObject); }
     void Start()
@@ -20,11 +25,18 @@ class GameManager : MonoBehaviour
         //         if (i.hasBurned) hasBurned++;
         //     var perc = hasBurned / flammables.Length * 100;
         // }
+        if (Input.GetKeyDown(KeyCode.C)) IncrementPerc();
+        T_Perc.gameObject.transform.localScale = Vector2.Lerp(T_Perc.gameObject.transform.localScale, Vector2.one, bumpSpeed);
     }
     public void IncrementPerc()
     {
         hasBurned++;
-        var perc = hasBurned / flammables.Length * 100;
-        T_Perc.text = perc + "%";
+        float perc = hasBurned / 10;
+        if (perc <= 1)
+        {
+            T_Perc.SetText("{0:0}%", perc * 100);
+            T_Perc.gameObject.transform.localScale *= bumpScaleFactor;
+            T_Perc.color = new Color(T_Perc.color.r, T_Perc.color.g * (1 - perc), T_Perc.color.b);
+        }
     }
 }
